9a7ee5249f2cbda6a5a64bd212caf08e8cd923bd
zgldh <zgldh@hotmail.com>
2017-07-11 20:53:45 +0800

    update menu

--- templates/init/resources/views/admin/menu.blade.php
+++ templates/init/resources/views/admin/menu.blade.php
@@ -4,13 +4,13 @@
 
 <router-treeview title="用户管理" icon="fa fa-users" :match="['/user']">
     <router-link tag="li" to="/user" exact>
-        <a><i class="fa fa-user"></i> 管理员列表</a>
+        <a><i class="fa fa-user"></i> <span>用户列表</span></a>
     </router-link>
     <router-link tag="li" to="/user/role">
-        <a><i class="fa fa-circle-o"></i> 管理员角色</a>
+        <a><i class="fa fa-circle-o"></i> <span>用户角色</span></a>
     </router-link>
     <router-link tag="li" to="/user/permission">
-        <a><i class="fa fa-circle-o"></i> 管理员权限</a>
+        <a><i class="fa fa-circle-o"></i> <span>用户权限</span></a>
     </router-link>
 </router-treeview>
 
