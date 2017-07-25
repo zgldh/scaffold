4a65a489a2aff626180d87036d3d8e3fab094cf3
Zhang Wenbin <zhangwb@shinetechchina.com>
2017-07-25 16:40:14 +0800

    editor page error messages

--- templates/init/resources/assets/js/commons/EditorHelper.js
+++ templates/init/resources/assets/js/commons/EditorHelper.js
@@ -1,10 +1,76 @@
+import _ from 'lodash';
+
 export var mixin = {
   data: function () {
-    return {};
+    return {
+      errors: {},
+      missingErrors: [],
+      saving: false
+    };
   },
   beforeMount: function () {
   },
   mounted: function () {
   },
-  methods: {}
+  methods: {
+    onSave: function (event) {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          this.saving = true;
+          this.errors = {};
+          this.missingErrors = [];
+          axios.post(this.resource, this.form)
+            .then(result => {
+              console.log(result);
+              this.saving = false;
+            })
+            .catch(({response}) => {
+              if (response.status == 422) {
+                // this.$refs.form.$children
+                this._distributeErrorMessages(response.data);
+              }
+              console.log('error', this.$refs.form, response);
+              this.saving = false;
+            })
+        }
+      });
+    },
+    _distributeErrorMessages(errors){
+      this.missingErrors = [];
+      let firstField = null;
+      _.forEach(errors, (errorArray, fieldName) => {
+        let field = FindFieldComponent(fieldName, this.$refs.form);
+        if (!firstField) {
+          firstField = field;
+        }
+        if (field) {
+          this.errors[fieldName] = errorArray[0];
+        }
+        else {
+          if (!_.isArray(errorArray)) {
+            errorArray = [errorArray];
+          }
+          this.missingErrors.push.apply(this.missingErrors, errorArray);
+        }
+      });
+
+      if (this.missingErrors.length) {
+        this.$el.scrollIntoView({block: "start", behavior: "smooth"});
+      }
+      else {
+        firstField.$el.scrollIntoView({block: "start", behavior: "smooth"});
+      }
+    }
+  }
 };
+
+function FindFieldComponent (fieldName, form) {
+  let field = null;
+  for (let i = 0; i < form.$children.length; i++) {
+    field = form.$children[i];
+    if (field.prop === fieldName) {
+      break;
+    }
+  }
+  return field;
+}
