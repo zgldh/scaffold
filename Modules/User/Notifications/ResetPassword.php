4bb4a913bf55493f7b2e6f4b65ea9798818edfad
Zhang Wenbin <zhangwb@shinetechchina.com>
2018-07-06 18:49:44 +0800

    Fixed reset password function

--- Modules/User/Notifications/ResetPassword.php
+++ Modules/User/Notifications/ResetPassword.php
@@ -17,7 +17,7 @@ class ResetPassword extends Notification
     /**
      * Create a notification instance.
      *
-     * @param  string  $token
+     * @param  string $token
      * @return void
      */
     public function __construct($token)
@@ -28,7 +28,7 @@ class ResetPassword extends Notification
     /**
      * Get the notification's channels.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return array|string
      */
     public function via($notifiable)
@@ -39,14 +39,19 @@ class ResetPassword extends Notification
     /**
      * Build the mail representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable)
     {
-        $fontEndUrl  = request()->headers->get('referer');
+        $frontendReferer = parse_url(request()->headers->get('referer'));
+        $frontendURL = $frontendReferer['scheme'] . '://' . $frontendReferer['host'];
+        if ($frontendReferer['port']) {
+            $frontendURL .= (':' . $frontendReferer['port']);
+        }
+        $frontendURL .= $frontendReferer['path'];
         $email = request()->get('email');
-        $resetUrl = $fontEndUrl . '#/password/reset?email='. base64_encode($email) .'&reset_token='.$this->token;
+        $resetUrl = $frontendURL . '#/password/reset?email=' . $email . '&reset_token=' . $this->token;
         return (new MailMessage)
             ->line('You are receiving this email because we received a password reset request for your account.')
             ->action('Reset Password', $resetUrl)
